82cd8e576bab8ef52e642dc8f16078acbf97e541
MUHETO Hodal <mhthodol@gmail.com>
2025-05-31 11:54:42 +0200

    I have created a form that will accept input and save them and retrieve the content to the user, this is fantastic ever

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def home(request):
     tasks = Task.objects.all()
-    return render(request, 'home.html', {'tasks': tasks})
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # Reload the page
+
+    return render(request, 'home.html', {'tasks': tasks, 'form': form})
