9d79d3a9d69d51ce859b106fd85b422672ca4d83
MUHETO Hodal <mhthodol@gmail.com>
2025-05-31 12:02:38 +0200

    unbelivable am able to delete the list of todo app whoooooaaa

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.shortcuts import get_object_or_404
 
 def home(request):
     tasks = Task.objects.all()
@@ -13,3 +14,9 @@ def home(request):
             return redirect('/')  # Reload the page
 
     return render(request, 'home.html', {'tasks': tasks, 'form': form})
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('/')
